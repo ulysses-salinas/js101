1bdc9bdf6ca9a4c9df07769068ae2f340b9089ec
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-01-29 03:00:04 -0600

    adding answers to 104-strings.js

--- exercises/104-strings.js
+++ exercises/104-strings.js
@@ -5,16 +5,20 @@
 // 1) Create a function "helloWorld"
 // 2) Return the string "Hello, world!"
 
+function helloWorld (){
+return "Hello, world!"
 
+
+}
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "helloName" that accepts 1 parameter (arity of 1)
 // 2) Use the symbol "name" for the parameter name
 // 3) Return the string "Hello, <name>!" where <name> is the value passed to the function
 
-function helloName (name) {
-
+function helloName (name){
+return "Hello, " + name + "!"
 }
-
+helloName("Bob")
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Return the length of the string "tarPitAbstract" defined below.
 // HINT: use the .length method
@@ -25,7 +29,7 @@ function abstractLength () {
     'Following Brooks we distinguish accidental from essential difficulty, but disagree with his premise that most complexity remaining in contemporary systems is essential. ' +
     'We identify common causes of complexity and discuss general approaches which can be taken to eliminate them where they are accidental in nature. ' +
     'To make things more concrete we then give an outline for a potential complexity-minimizing approach based on functional programming and Codd’s relational model of data.'
-
+return tarPitAbstract.length
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -34,10 +38,9 @@ function abstractLength () {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 
 function makeLoud () {
-  const chorus = 'Who let the dogs out?'
-
+  const chorus = 'Who let the dogs out?';
+  return chorus.toUpperCase();
 }
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Assume that a string is passed to the parameter "str" in the function below.
 // Return the value of "str" in all lower case letters.
@@ -45,5 +48,5 @@ function makeLoud () {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
 
 function makeQuiet (str) {
-
+return str.toLowerCase();
 }
